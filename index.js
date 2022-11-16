519af1139fffaa98a7969973cb1e64ba89781e81
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-11-16 14:52:31 +0600

    added

--- index.js
+++ index.js
@@ -19,11 +19,14 @@ async function run() {
     try {
         const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
         const bookingCollection = client.db('doctorsPortal').collection('bookings')
+
         //api get
         app.get('/appointmentOptions', async (req, res) => {
             const date = req.query.date;
             const query = {}
             const options = await appointmentOptionCollection.find(query).toArray();
+
+            // get the booking of the provided date
             const bookingQuery = { appointmentDate: date }
             const alreadyBooked = await bookingCollection.find(bookingQuery).toArray();
 
@@ -36,10 +39,69 @@ async function run() {
             })
             res.send(options);
         });
+        //--------------//
+        app.get('/v2/appointmentOptions', async (req, res) => {
+            const date = req.query.date;
+            const options = await appointmentOptionCollection.aggregate([
+                {
+                    $lookup: {
+                        from: 'bookings',
+                        localField: 'name',
+                        foreignField: 'treatment',
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $eq: ['$appointmentDate', date]
+                                    }
+                                }
+                            }
+                        ],
+                        as: 'booked'
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        slots: 1,
+                        booked: {
+                            $map: {
+                                input: '$booked',
+                                as: 'book',
+                                in: '$$book.slot'
+                            }
+                        }
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        slots: {
+                            $setDifference: ['$slots', '$booked']
+                        }
+                    }
+                }
+            ]).toArray();
+            res.send(options)
+        })
+
 
         //api post
         app.post('/bookings', async (req, res) => {
             const booking = req.body
+            console.log(booking)
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email: booking.email,
+                treatment: booking.treatment
+            }
+
+            const alreadyBooked = await bookingCollection.find(query).toArray();
+
+            if (alreadyBooked.length) {
+                const message = `You already have a booking on ${booking.appointmentDate}`
+                return res.send({ acknowledged: false, message })
+            }
             const result = await bookingCollection.insertOne(booking);
             res.send(result)
         })
