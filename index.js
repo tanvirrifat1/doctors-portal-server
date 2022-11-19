864ee1aef6760d3c2032fcf6627b25da181b0770
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-11-19 14:01:00 +0600

    added more

--- index.js
+++ index.js
@@ -41,6 +41,20 @@ async function run() {
         const usersCollection = client.db('doctorsPortal').collection('users')
         const doctorsCollection = client.db('doctorsPortal').collection('doctors')
 
+        // Note: make sure use verifyAdmin after verifyJWT
+        const verifyAdmin = async (req, res, next) => {
+            console.log('inside verify', req.decoded.email)
+
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersCollection.findOne(query);
+
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            next()
+        }
+
         // //api get
         app.get('/appointmentOptions', async (req, res) => {
             const date = req.query.date;
@@ -228,14 +242,7 @@ async function run() {
             res.send(result)
         });
         //----------//
-        app.put('/users/admin/:id', verifyJWT, async (req, res) => {
-            const decodedEmail = req.decoded.email;
-            const query = { email: decodedEmail };
-            const user = await usersCollection.findOne(query);
-
-            if (user?.role !== 'admin') {
-                return res.status(403).send({ message: 'forbidden access' })
-            }
+        app.put('/users/admin/:id', verifyJWT, verifyAdmin, async (req, res) => {
 
             const id = req.params.id;
             const filter = { _id: ObjectId(id) }
@@ -250,16 +257,23 @@ async function run() {
         });
 
         //-----DoctorsGetApi-----//
-        app.get('/doctors', async (req, res) => {
+        app.get('/doctors', verifyJWT, verifyAdmin, async (req, res) => {
             const query = {};
             const doctors = await doctorsCollection.find(query).toArray();
             res.send(doctors)
         })
 
         //-----DoctorImageApi-----//
-        app.post('/doctors', async (req, res) => {
+        app.post('/doctors', verifyJWT, verifyAdmin, async (req, res) => {
             const doctor = req.body;
             const result = await doctorsCollection.insertOne(doctor);
+            res.send(result)
+        });
+        //--------deleteApi-------//
+        app.delete('/doctors/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await doctorsCollection.deleteOne(filter);
             res.send(result)
         })
 
